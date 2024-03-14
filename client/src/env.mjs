a69b301e753e96a570a9174915fc5341d135f45b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-03-14 17:09:32 +0100

    Basic auth

--- client/src/env.mjs
+++ client/src/env.mjs
@@ -2,12 +2,17 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {},
+  server: {
+    BASIC_AUTH_USER: z.string(),
+    BASIC_AUTH_PASSWORD: z.string(),
+  },
   client: {
     NEXT_PUBLIC_URL: z.string().url(),
     NEXT_PUBLIC_ARCGIS_API_KEY: z.string(),
   },
   runtimeEnv: {
+    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
+    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NEXT_PUBLIC_ARCGIS_API_KEY: process.env.NEXT_PUBLIC_ARCGIS_API_KEY,
   },
