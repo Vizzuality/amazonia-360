3a2f91dcc6e003ea7546d06c26e162ad5d95f2aa
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-03-15 11:01:06 +0100

    Charts: arc

---
+++ client/src/components/charts/arc.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useMemo } from "react";
+
+import { Group } from "@visx/group";
+import { useParentSize } from "@visx/responsive";
+import { scaleLinear } from "@visx/scale";
+import { Arc } from "@visx/shape";
+
+import { formatPercentage } from "@/lib/formats";
+
+export type ArcChartProps = {
+  value: number;
+  color?: `#${string}`;
+};
+
+export default function ArcChart({ value, color = "#FFDA00" }: ArcChartProps) {
+  const { parentRef, width, height } = useParentSize({ debounceTime: 150 });
+
+  const startAngle = -Math.PI / 2;
+  const endAngle = Math.PI / 2;
+
+  const xScale = useMemo(() => {
+    return scaleLinear<number>({
+      domain: [0, 1],
+      range: [startAngle, endAngle],
+      clamp: true,
+    });
+  }, [startAngle, endAngle]);
+
+  const { format } = formatPercentage({});
+
+  return (
+    <div ref={parentRef} className="w-full h-28">
+      <svg width={width} height={height}>
+        <Group top={height} left={width / 2}>
+          <Arc
+            startAngle={xScale(0)}
+            endAngle={xScale(1)}
+            innerRadius={(height * 3) / 4}
+            outerRadius={height}
+            cornerRadius={height / 4}
+            fill={color}
+            fillOpacity={0.25}
+          />
+          <Arc
+            startAngle={xScale(0)}
+            endAngle={xScale(value)}
+            innerRadius={(height * 3) / 4}
+            outerRadius={height}
+            cornerRadius={height / 4}
+            fill={color}
+          />
+        </Group>
+      </svg>
+      <span className="block -translate-y-full text-center text-4xl">
+        {format(value)}
+      </span>
+    </div>
+  );
+}
---
+++ client/src/lib/formats.ts
@@ -0,0 +1,8 @@
+export const formatPercentage = (options: Intl.NumberFormatOptions) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "percent",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+    ...options,
+  });
+};
