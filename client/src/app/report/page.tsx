dcf8c8457d9f11a140de6890cae4280b3f19d7f3
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-03-20 10:08:58 +0100

    Location: background for visibility

--- client/src/app/report/page.tsx
+++ client/src/app/report/page.tsx
@@ -4,6 +4,7 @@ import Map from "@/containers/report/map";
 export default function ReportPage() {
   return (
     <main className="relative flex flex-col h-[calc(100svh_-_theme(space.20))]">
+      {/* <div className="absolute z-10 w-1/2 h-full left-0 top-0 bg-gradient-to-r from-slate-500/20 to-slate-500/0 pointer-events-none"></div> */}
       <div className="absolute top-1/2 left-0 z-10 w-full pointer-events-none -translate-y-1/2">
         <div className="container">
           <ReportLocation />
