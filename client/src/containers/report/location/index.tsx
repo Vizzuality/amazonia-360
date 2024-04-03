223a3002a66e04785ac0c9ce80419e1c08b74dda
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-04-03 10:34:49 +0200

    Location: Buttons

--- client/src/containers/report/location/index.tsx
+++ client/src/containers/report/location/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Confirm from "@/containers/report/location/confirm";
 import Search from "@/containers/report/location/search";
 import Sketch from "@/containers/report/location/sketch";
