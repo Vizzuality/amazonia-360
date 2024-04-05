56947521f1b566c7e6a67a05013067c52d207e08
Biel Stela <biel.stela@gmail.com>
2024-04-05 10:41:45 +0200

    Refactor project structure

--- api/app/config/config.py
+++ api/app/config/config.py
@@ -1,8 +1,13 @@
+"""Global configuration for the application."""
+
 from functools import lru_cache
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """Global configuration for the application."""
+
     postgres_user: str
     postgres_password: str
     postgres_db: str
@@ -10,10 +15,11 @@ class Settings(BaseSettings):
     postgres_port: str
     auth_token: str
 
-    class Config:
+    class Config:  # noqa: D106
         env_file = ".env"
 
 
 @lru_cache
 def get_settings() -> Settings:
+    """Return the global configuration."""
     return Settings()
