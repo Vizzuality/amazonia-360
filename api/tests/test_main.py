11b33856840dae27aeedb1b4982b3b41a0e94053
Biel Stela <biel.stela@gmail.com>
2024-04-08 10:18:42 +0200

    Styles tests and pin pytest requirements.txt

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,8 +1,10 @@
 import os
+import shutil
+
 import pytest
 from app.config.config import get_settings
+
 from tests.utils import test_client
-import shutil
 
 token = get_settings().auth_token
 test_tif_path = get_settings().tif_path
@@ -21,12 +23,12 @@ geojson = {
                         [115.0870, -8.3355],
                         [115.0970, -8.3355],
                         [115.0970, -8.3455],
-                        [115.0870, -8.3455]
+                        [115.0870, -8.3455],
                     ]
-                ]
-            }
+                ],
+            },
         }
-    ]
+    ],
 }
 
 headers = {"Authorization": f"Bearer {token}"}
@@ -35,7 +37,7 @@ headers = {"Authorization": f"Bearer {token}"}
 def test_no_token():
     response = test_client.get("/tifs")
     assert response.status_code == 401
-    assert response.text == 'Unauthorized'
+    assert response.text == "Unauthorized"
 
 
 def test_with_token(setup_data_folder):
@@ -55,14 +57,23 @@ def test_list_files(setup_files):
     assert response.json() == {"files": files}
 
 
+def test_wrong_file_name_raises_404(setup_data_folder):
+    response = test_client.post(
+        "/exact_zonal_stats", headers=headers, params={"raster_filename": "wrong.tif"}, json=geojson
+    )
+    assert response.status_code == 404
+
 
 # TODO: Activate this test once having a test tif file
 
 # def test_exact_zonal_stats(setup_tif):
-#     response = test_client.post("/exact_zonal_stats", headers=headers, params={"raster_name": "test.tif"}, json=geojson)
+#     response = test_client.post(
+#       "/exact_zonal_stats", headers=headers, params={"raster_name": "test.tif"}, json=geojson
+#     )
 #     assert response.status_code == 200
 #     assert response.json() == {"mean": 0.0, "min": 0.0, "max": 0.0, "count": 0, "sum": 0.0}
 #
+
 
 @pytest.fixture()
 def setup_data_folder():
@@ -77,22 +88,22 @@ def setup_data_folder():
 def setup_files():
     os.mkdir(get_settings().tif_path)
     for file in files:
-        with open(f'{test_tif_path}/{file}', 'w') as f:
-            f.write('test data')
+        with open(f"{test_tif_path}/{file}", "w") as f:
+            f.write("test data")
 
     yield
 
     for file in files:
-        os.remove(f'{test_tif_path}/{file}')
+        os.remove(f"{test_tif_path}/{file}")
     os.rmdir(test_tif_path)
 
 
 @pytest.fixture()
 def setup_tif():
     os.mkdir(get_settings().tif_path)
-    shutil.copy('/opt/api/tests/test.tif', get_settings().tif_path)
+    shutil.copy("/opt/api/tests/test.tif", get_settings().tif_path)
 
     yield
 
-    os.remove(f'{get_settings().tif_path}/test.tif')
+    os.remove(f"{get_settings().tif_path}/test.tif")
     os.rmdir(get_settings().tif_path)
