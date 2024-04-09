f26ecf6e631104204686c5f3917e20479a34b632
Biel Stela <biel.stela@gmail.com>
2024-04-09 09:33:45 +0200

    Format config.py and update pyproject to new format

--- api/app/config/config.py
+++ api/app/config/config.py
@@ -1,19 +1,16 @@
 """Global configuration for the application."""
-import os
+
 from functools import lru_cache
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     """Global configuration for the application."""
 
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
-    postgres_host: str
-    postgres_port: str
     auth_token: str
     tiff_path: str
+
 
 @lru_cache
 def get_settings() -> Settings:
