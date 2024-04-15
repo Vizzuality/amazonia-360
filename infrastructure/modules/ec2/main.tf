05170d026ac589b4fe6ed5a6b4100524dd34e335
alexeh <alexurola@gmail.com>
2024-04-15 07:27:33 +0300

    switch to vpc_security_group_ids

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "amazonia360-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  security_groups = var.security_groups
+  vpc_security_group_ids = var.security_group_ids
   key_name = aws_key_pair.local_public_key.key_name
   associate_public_ip_address = true
 
