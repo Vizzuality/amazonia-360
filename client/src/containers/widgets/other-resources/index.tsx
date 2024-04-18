0ad34d808ec3ca2599cfacee7e669909dfd1b594
anamontiaga <amontiaga@gmail.com>
2024-04-18 10:47:01 +0200

    card and types

--- client/src/containers/widgets/other-resources/index.tsx
+++ client/src/containers/widgets/other-resources/index.tsx
@@ -1,86 +1,122 @@
-import { Files, FileVideo2, DatabaseIcon } from "lucide-react";
+import { useState } from "react";
 
-import { Card } from "@/containers/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { Button } from "@/components/ui/button";
+import Resource from "./resource";
+import { type ResourcesProps } from "./types";
 
 export default function OtherResources() {
-  type ResourcesProps = {
-    title: string;
-    author: string;
-    type: "database" | "publication" | "multimedia";
-  }[];
+  const [tab, setTab] = useState("all");
 
-  const RESOURCES: ResourcesProps = [
-    {
-      title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
-      author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
-      type: "publication",
-    },
-    {
-      title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
-      author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
-      type: "database",
-    },
-    {
-      title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
-      author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
-      type: "multimedia",
-    },
-    {
-      title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
-      author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
-      type: "publication",
-    },
-  ];
+  const RESOURCES: ResourcesProps = {
+    multimedia: [
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "document",
+      },
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "database",
+      },
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "multimedia",
+      },
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "document",
+      },
+    ],
+    database: [
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "document",
+      },
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "database",
+      },
+    ],
+    publications: [
+      {
+        title: "El teletrabajo en Bolivia: de la pandemia a la postpandemia",
+        author: "Serrate, Liliana; Urquidi, Manuel; Aramayo, Fernando",
+        type: "database",
+      },
+    ],
+  };
+
   return (
     <div className="container">
       <h2 className="text-xl font-semibold mb-4">Other resources</h2>
-      <div className="grid grid-cols-4 gap-2">
-        {RESOURCES.map((resource, idx) => (
-          <Card key={idx}>
-            <div className="flex flex-col space-y-4 text-center items-center">
-              <div className="rounded-full w-20 h-20 bg-blue-50 flex items-center justify-center">
-                {resource.type === "publication" && (
-                  <Files
-                    size={40}
-                    strokeWidth={1.5}
-                    className="text-blue-400"
-                  />
-                )}
-                {resource.type === "multimedia" && (
-                  <FileVideo2
-                    size={40}
-                    strokeWidth={1.5}
-                    className="text-blue-400"
-                  />
-                )}
-                {resource.type === "database" && (
-                  <DatabaseIcon
-                    size={40}
-                    strokeWidth={1.5}
-                    className="text-blue-400"
-                  />
-                )}
-              </div>
-              <div className="flex flex-col space-y-2">
-                <h3 className="text-base text-blue-500 font-semibold">
-                  {resource.title}
-                </h3>
-                <p className="text-xs text-gray-500 font-normal">
-                  Author: {resource.author}
-                </p>
-              </div>
-              <div className="flex space-x-4 w-full">
-                <Button variant="outline" className="w-1/2">
-                  Info
-                </Button>
-                <Button className="w-1/2">Download</Button>
-              </div>
-            </div>
-          </Card>
-        ))}
-      </div>
+      <Tabs defaultValue={tab}>
+        <TabsList className="w-full px-5">
+          <TabsTrigger
+            value="all"
+            className="rounded-l-lg"
+            onClick={() => setTab("all")}
+          >
+            All
+          </TabsTrigger>
+          <TabsTrigger
+            value="publications"
+            className="rounded-r-lg"
+            onClick={() => setTab("publications")}
+          >
+            Publications
+          </TabsTrigger>
+          <TabsTrigger
+            value="database"
+            className="rounded-l-lg"
+            onClick={() => setTab("database")}
+          >
+            Database
+          </TabsTrigger>
+          <TabsTrigger
+            value="multimedia"
+            className="rounded-r-lg"
+            onClick={() => setTab("multimedia")}
+          >
+            Multimedia
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <div className="grid grid-cols-4 gap-2">
+            {Object.values(RESOURCES)
+              .flat()
+              .map((r, idx) => (
+                <Resource key={idx} resource={r} />
+              ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="publications">
+          <div className="grid grid-cols-4 gap-2">
+            {RESOURCES.publications.map((r, idx) => (
+              <Resource key={idx} resource={r} />
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="database">
+          <div className="grid grid-cols-4 gap-2">
+            {RESOURCES.database.map((r, idx) => (
+              <Resource key={idx} resource={r} />
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="multimedia">
+          <div className="grid grid-cols-4 gap-2">
+            {RESOURCES.multimedia.map((r, idx) => (
+              <Resource key={idx} resource={r} />
+            ))}
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
