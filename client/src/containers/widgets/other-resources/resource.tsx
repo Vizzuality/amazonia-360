7203e4e101273697aef659e25df645988b905252
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-04-30 13:02:48 +0200

    Other resources: info and download

--- client/src/containers/widgets/other-resources/resource.tsx
+++ client/src/containers/widgets/other-resources/resource.tsx
@@ -10,10 +10,23 @@ import {
 import { Card } from "@/containers/card";
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 
 import { ResourceProps } from "./types";
 
-export default function Resource({ Name, Type, Topic }: ResourceProps) {
+export default function Resource({
+  Name,
+  Type,
+  Topic,
+  Description,
+  Year,
+  Month,
+  URL,
+  Author,
+  Country,
+  Department,
+  OrgUnit,
+}: ResourceProps) {
   return (
     <Card>
       <div className="flex flex-col grow space-y-4 text-center items-center justify-between">
@@ -66,10 +79,72 @@ export default function Resource({ Name, Type, Topic }: ResourceProps) {
         </header>
         <footer>
           <div className="flex space-x-4 w-full">
-            <Button variant="outline" className="w-1/2">
-              Info
-            </Button>
-            <Button className="w-1/2">Download</Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline" className="w-1/2">
+                  Info
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent>
+                <div className="flex flex-col space-y-4">
+                  <h3 className="text-lg text-blue-500 font-semibold pr-4">
+                    {Name}
+                  </h3>
+                  <p className="text-sm text-foreground font-medium">
+                    {Description}
+                  </p>
+                  <div className="text-xs text-foreground font-medium space-y-2">
+                    <p>
+                      <span className="font-semibold">Type:</span> {Type}
+                    </p>
+                    <p>
+                      <span className="font-semibold">Topic:</span> {Topic}
+                    </p>
+                    <p>
+                      <span className="font-semibold">Year:</span> {Year}
+                    </p>
+                    {Month && (
+                      <p>
+                        <span className="font-semibold">Month:</span> {Month}
+                      </p>
+                    )}
+                    <p>
+                      <span className="font-semibold">URL:</span>{" "}
+                      <a href={URL} target="_blank" rel="noreferrer">
+                        {URL}
+                      </a>
+                    </p>
+                    {Author && (
+                      <p>
+                        <span className="font-semibold">Author:</span> {Author}
+                      </p>
+                    )}
+                    <p>
+                      <span className="font-semibold">Country:</span> {Country}
+                    </p>
+                    <p>
+                      <span className="font-semibold">Department:</span>{" "}
+                      {Department}
+                    </p>
+                    <p>
+                      <span className="font-semibold">OrgUnit:</span> {OrgUnit}
+                    </p>
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
+
+            {!!URL && (
+              <a
+                className="inline-block w-1/2"
+                href={URL}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button className="w-full">Download</Button>
+              </a>
+            )}
           </div>
         </footer>
       </div>
