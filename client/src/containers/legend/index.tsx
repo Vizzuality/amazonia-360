5ab316546bf668becbc764a6a9323ad0f75c57d6
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-05-06 09:24:25 +0200

    Legend: chevron up and down

--- client/src/containers/legend/index.tsx
+++ client/src/containers/legend/index.tsx
@@ -1,18 +1,25 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@radix-ui/react-collapsible";
-import { LuChevronUp } from "react-icons/lu";
+import { LuChevronDown, LuChevronUp } from "react-icons/lu";
 
 export default function Legend({ children }: PropsWithChildren) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Collapsible className="bg-white border border-blue-100 rounded-3xl py-2 px-4">
+    <Collapsible
+      open={open}
+      onOpenChange={setOpen}
+      className="bg-white border border-blue-100 rounded-3xl py-2 px-4"
+    >
       <CollapsibleTrigger className="text-sm min-w-28 w-full flex justify-between items-center">
         <span>Legend</span>
-        <LuChevronUp className="w-4 h-4" />
+        {open && <LuChevronDown className="w-4 h-4" />}
+        {!open && <LuChevronUp className="w-4 h-4" />}
       </CollapsibleTrigger>
       <CollapsibleContent className="pt-2">{children}</CollapsibleContent>
     </Collapsible>
