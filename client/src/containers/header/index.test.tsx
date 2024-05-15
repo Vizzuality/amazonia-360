78b40ca21af34e9d1e2f4e4429fd5992cf411d58
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-05-15 13:08:13 +0200

    Header: tests

---
+++ client/src/containers/header/index.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+
+import Header from "@/containers/header";
+
+const mockUsePathname = jest.fn<string, unknown[]>();
+jest.mock("next/navigation", () => ({
+  usePathname() {
+    return mockUsePathname();
+  },
+}));
+
+describe("Header", () => {
+  beforeEach(() => {
+    mockUsePathname.mockReturnValue("/");
+  });
+
+  it("renders Header component without crashing", () => {
+    render(<Header />);
+    expect(screen.getByRole("banner")).toBeInTheDocument();
+  });
+
+  it("renders the title", () => {
+    render(<Header />);
+    expect(screen.getByText("Amazonia360")).toBeInTheDocument();
+  });
+
+  it("renders the logo", () => {
+    render(<Header />);
+    expect(screen.getByAltText("IDB")).toBeInTheDocument();
+  });
+
+  it("renders the badge", () => {
+    render(<Header />);
+    expect(screen.getByText("Prototype")).toBeInTheDocument();
+  });
+
+  // Selected
+  it("renders the Home link selected", () => {
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders the Report Tool link selected", () => {
+    mockUsePathname.mockReturnValue("/report");
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders the Hub link selected", () => {
+    mockUsePathname.mockReturnValue("/hub");
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+});
