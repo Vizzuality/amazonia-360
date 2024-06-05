dfbfcdc86b2351d478413c97e498653c7382f39f
Biel Stela <biel.stela@gmail.com>
2024-06-05 17:49:50 +0200

    Add router to main app and fix dependencies

--- api/app/routers/h3.py
+++ api/app/routers/h3.py
@@ -4,21 +4,23 @@ import h3
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
-h3index_router = APIRouter()
+from app.config.config import get_settings
+
+h3tile_router = APIRouter()
 
 
-@h3index_router.get(
+@h3tile_router.get(
     "/tile/{h3index}",
-    responses={200: {"description": "Return a tile"}, 404: {"description": "Not found"}},
+    responses={200: {"description": "Get a grid tile"}, 404: {"description": "Not found"}},
     response_model=None,
 )
-async def h3_tile(tile_index: str) -> FileResponse:
+async def grid_tile(tile_index: str) -> FileResponse:
     """Request a tile of h3 cells
 
     :raises HTTPException 404: Item not found
     """
     z = h3.get_resolution(tile_index)
-    tile_file = f"/home/biel/Vizzuality/experiments/h3-tiler/data/amazonia360-raw/pop/{z}/{tile_index}.arrow"
+    tile_file = os.path.join(get_settings().grid_tiles_path, f"{z}/{tile_index}.arrow")
     if not os.path.exists(tile_file):
         raise HTTPException(status_code=404, detail="Tile not found")
     return FileResponse(tile_file, media_type="application/octet-stream")
