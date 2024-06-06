25f97f70412fa2729dc5cc21f044e86dd18ee00b
Biel Stela <biel.stela@gmail.com>
2024-06-06 12:19:59 +0200

    script for base case

--- science/src/to_tile_files.py
+++ science/src/to_tile_files.py
@@ -11,9 +11,9 @@ import typer
 from h3ronpy.polars.raster import nearest_h3_resolution, raster_to_dataframe
 from rasterio.windows import Window
 from rich import print
-from rich.progress import track
+from rich.progress import Progress
 
-app = typer.Typer(pretty_exceptions_short=True, pretty_exceptions_show_locals=False)
+cli = typer.Typer(pretty_exceptions_short=True, pretty_exceptions_show_locals=False)
 
 
 class AvailableAggFunctions(str, Enum):  # noqa: D101
@@ -81,7 +81,7 @@ def aggregate_cells(
     )
 
 
-@app.command()
+@cli.command()
 def main(
     input_file: Path,
     output_path: Path,
@@ -109,76 +109,115 @@ def main(
             if h3_res is not None
             else nearest_h3_resolution(src.shape, src.transform)
         )
-        print(f"Will process {splits ** 2} chunks at h3 resolution {h3res}")
+        n_chunks = splits**2
 
-        # iterate over raster chunks
-        for i, (_, window) in enumerate(chunk_generator(splits, src.height, src.width)):
-            print(f"Processing chunk {i + 1}/{splits ** 2} ...")
-            data = src.read(1, window=window)
-            win_transform = src.window_transform(window)
-            nodata = nodata if nodata is not None else src.nodata
-            df = raster_to_dataframe(
-                data,
-                win_transform,
-                h3res,
-                nodata_value=nodata,
-                compact=False,
-            )
+        # Resolution of the tile index. A tile is a h3 cell that contains all the
+        # cells that are 5 resolutions below it.
+        tile_index_res = h3res - 5
 
-            # Resolution of the tile index. A tile is a h3 cell that contains all the
-            # cells that are 5 resolutions below it.
-            tile_index_res = h3res - 5
+        output_path_base = output_path / str(tile_index_res)
+        output_path_base.mkdir(exist_ok=True, parents=True)
 
-            df = (
-                df.with_columns(
-                    pl.col("cell").h3.change_resolution(tile_index_res).alias("tile")
-                )
-                .filter(
-                    pl.col("value") > 0
-                )  # NOTE: should I do this? should it be dealt with nodata?
-                .unique(subset=["cell"])
-            )
-            if use_hex:
-                df = df.with_columns(
-                    pl.col("cell").cast(pl.Utf8).h3.cells_parse().h3.cells_to_string()
-                )
+        with Progress(transient=True) as progress:
+            read_chunk_task = progress.add_task("Sampling raster to h3", total=n_chunks)
 
-        while tile_index_res >= 0:
-            overview_res = tile_index_res + 5
-
-            if (
-                overview_res < h3res
-            ):  # aggregate to correct overview resolution if not the first write
-                print(f"Aggregating to resolution {overview_res}")
-                df = aggregate_cells(
-                    df, overview_res, agg_func.value, h3index_col_name="cell"
-                ).with_columns(
-                    pl.col("cell").h3.change_resolution(tile_index_res).alias("tile")
-                )
-
-            overview_output_path = output_path / str(tile_index_res)
-            overview_output_path.mkdir(exist_ok=True, parents=True)
-
-            # make tiles
-            partition_dfs = df.partition_by(["tile"], as_dict=True, include_key=False)
-
-            for tile_group, tile_df in track(
-                partition_dfs.items(), description="Writing tiles"
+            for i, (_, window) in enumerate(
+                chunk_generator(splits, src.height, src.width)
             ):
-                if tile_df.shape[0] == 0:  # todo: skip empty tiles ?
-                    continue
-                tile_id = tile_group[0]
-                filename = overview_output_path / (hex(tile_id)[2:] + ".arrow")
-                if tile_id in seen_tiles:
-                    pl.concat([pl.read_ipc(filename), tile_df]).unique(
-                        subset=["cell"]
-                    ).write_ipc(filename)
-                else:
-                    tile_df.write_ipc(filename)
-                seen_tiles.add(tile_id)
+                progress.update(
+                    read_chunk_task, description=f"Processing chunk {i+1} of {n_chunks}"
+                )
+                data = src.read(1, window=window)
+                win_transform = src.window_transform(window)
+                nodata = nodata if nodata is not None else src.nodata
+                df = raster_to_dataframe(
+                    data,
+                    win_transform,
+                    h3res,
+                    nodata_value=nodata,
+                    compact=False,
+                )
 
-            tile_index_res -= 1
+                df = (
+                    df.with_columns(
+                        pl.col("cell")
+                        .h3.change_resolution(tile_index_res)
+                        .alias("tile")
+                    )
+                    .filter(
+                        pl.col("value") > 0
+                    )  # NOTE: should I do this? should it be dealt with nodata?
+                    .unique(subset=["cell"])
+                )
+                if use_hex:
+                    df = df.with_columns(
+                        pl.col("cell")
+                        .cast(pl.Utf8)
+                        .h3.cells_parse()
+                        .h3.cells_to_string()
+                    )
+
+                    # make tiles
+                partition_dfs = df.partition_by(
+                    ["tile"], as_dict=True, include_key=False
+                )
+
+                write_task = progress.add_task(
+                    "Writing tiles", total=len(partition_dfs.items()), visible=True
+                )
+                for tile_group, tile_df in partition_dfs.items():
+                    tile_id = tile_group[0]
+                    filename = output_path_base / (hex(tile_id)[2:] + ".arrow")
+                    if tile_id in seen_tiles:
+                        pl.concat([pl.read_ipc(filename), tile_df]).unique(
+                            subset=["cell"]
+                        ).write_ipc(filename)
+                    else:
+                        tile_df.write_ipc(filename)
+                    seen_tiles.add(tile_id)
+                    progress.update(write_task, advance=1)
+
+                progress.update(write_task, visible=False)
+                progress.update(read_chunk_task, advance=1)
+
+        print("")
+
+        # while tile_index_res >= 0:
+        #     overview_res = tile_index_res + 5
+        #
+        #     if (
+        #         overview_res < h3res
+        #     ):  # aggregate to correct overview resolution if not the first write
+        #         print(f"Aggregating to resolution {overview_res}")
+        #         df = aggregate_cells(
+        #             df, overview_res, agg_func.value, h3index_col_name="cell"
+        #         ).with_columns(
+        #             pl.col("cell").h3.change_resolution(tile_index_res).alias("tile")
+        #         )
+        #
+        #     overview_output_path = output_path / str(tile_index_res)
+        #     overview_output_path.mkdir(exist_ok=True, parents=True)
+        #
+        #     # make tiles
+        #     partition_dfs = df.partition_by(["tile"], as_dict=True, include_key=False)
+        #
+        #     for tile_group, tile_df in track(
+        #         partition_dfs.items(), description="Writing tiles"
+        #     ):
+        #         if tile_df.shape[0] == 0:  # todo: skip empty tiles ?
+        #             continue
+        #         tile_id = tile_group[0]
+        #         filename = overview_output_path / (hex(tile_id)[2:] + ".arrow")
+        #         if tile_id in seen_tiles:
+        #             pl.concat([pl.read_ipc(filename), tile_df]).unique(
+        #                 subset=["cell"]
+        #             ).write_ipc(filename)
+        #         else:
+        #             tile_df.write_ipc(filename)
+        #         seen_tiles.add(tile_id)
+        #
+        #     tile_index_res -= 1
 
 
 if __name__ == "__main__":
-    app()
+    cli()
