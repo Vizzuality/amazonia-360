2716bd6b6baa7bb49dbc80fc0b1a63a2ad49861f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-06-18 16:16:53 +0200

    h3 tile layer: datafilter first step

--- client/src/app/grid/page.tsx
+++ client/src/app/grid/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import PageProviders from "@/app/grid/page-providers";
 
 import Map from "@/containers/grid/map";
+import Sidebar from "@/containers/grid/sidebar";
 
 export const metadata: Metadata = {
   title: "Grid | location",
@@ -14,11 +15,10 @@ export default function GridPage() {
     <PageProviders>
       <main className="relative flex flex-col min-h-[calc(100svh_-_theme(space.40)_+_1px)]">
         {/* <div className="absolute z-10 w-1/2 h-full left-0 top-0 bg-gradient-to-r from-slate-500/20 to-slate-500/0 pointer-events-none"></div> */}
-        <div className="absolute top-1/2 left-0 z-10 w-full pointer-events-none -translate-y-1/2">
-          <div className="container">Grid</div>
+        <div className="flex justify-between items-stretch grow">
+          <Sidebar />
+          <Map />
         </div>
-
-        <Map />
       </main>
     </PageProviders>
   );
