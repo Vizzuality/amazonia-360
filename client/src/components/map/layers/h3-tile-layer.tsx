95223b439907c95fa93f0a76bed677cb15dec9a7
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-06-18 09:41:22 +0200

    Deckgl integrated. Not working

---
+++ client/src/components/map/layers/h3-tile-layer.tsx
@@ -0,0 +1,144 @@
+import { TileLayer, _Tileset2D as Tileset2D } from "@deck.gl/geo-layers/typed";
+import bbox from "@turf/bbox";
+import { lineString } from "@turf/helpers";
+import {
+  cellToBoundary,
+  cellToParent,
+  edgeLength,
+  getResolution,
+  latLngToCell,
+  originToDirectedEdges,
+  polygonToCells,
+} from "h3-js";
+
+/** Fills the viewport bbox polygon(s) with h3 cells */
+function fillViewportBBoxes(bboxes: number[][], tileRes: number) {
+  let cells: string[] = [];
+  for (const bbox of bboxes) {
+    const poly = [
+      [bbox[0], bbox[1]],
+      [bbox[0], bbox[3]],
+      [bbox[2], bbox[3]],
+      [bbox[2], bbox[1]],
+      [bbox[0], bbox[1]],
+    ];
+    cells = cells.concat(polygonToCells(poly, tileRes, true));
+  }
+  return cells;
+}
+
+/** Splits viewport bounds into two if span is > 180 and adds buffer to include border hexagons.
+ * Also clamps viewport bounds to the world bounds [-+180, -+90], which maybe backfires if used in GlobeView
+ * */
+function makeBufferedBounds(bounds: number[], tileRes: number) {
+  // getHexagonEdgeLengthAvg for a given resolution doesn't have "rads"
+  // as a unit option, even that the docs say it does >:(.
+  // So will use random edge from the central cell as an edge length proxy since it doesn't need to be exact.
+  const medLat = (bounds[1] + bounds[3]) / 2;
+  const medLng = (bounds[0] + bounds[2]) / 2;
+  const centroidCellEdges = originToDirectedEdges(
+    latLngToCell(medLat, medLng, tileRes),
+  );
+  // largest edge from the center cell of the viewport
+  const buffer =
+    (Math.max(...centroidCellEdges.map((x) => edgeLength(x, "rads"))) * 180) /
+    Math.PI;
+  bounds[0] = Math.max(bounds[0] - buffer, -180); // min X
+  bounds[1] = Math.max(bounds[1] - buffer, -90); // min Y
+  bounds[2] = Math.min(bounds[2] + buffer, 180); // max X
+  bounds[3] = Math.min(bounds[3] + buffer, 90); // max Y
+  // polygons spanning more than 180 degrees need to be split in two parts to be correctly covered by h3
+  // https://github.com/uber/h3-js/issues/180#issuecomment-1652453683
+  // So split the bounds in two parts
+  if (bounds[2] - bounds[0] > 180) {
+    const bounds2 = [...bounds];
+    const xSplitPoint = (bounds[2] - bounds[0]) / 2;
+    // new min and max X at split point
+    bounds[2] -= xSplitPoint;
+    bounds2[0] += xSplitPoint;
+    return [bounds, bounds2];
+  } else {
+    return [bounds];
+  }
+}
+
+export class H3Tileset2D extends Tileset2D {
+  /** Returns true if the tile is visible in the current viewport
+   * FIXME: Should be adapted to h3 tiles. How? no idea...
+   * */
+  isTileVisible(tile, cullRect) {
+    return super.isTileVisible(tile, cullRect);
+  }
+
+  /** Returns the tile indices that are needed to cover the viewport
+   * It tries to get a sensible number of tiles by computing the h3 resolution
+   * that will be used to cover the viewport in h3 cells.
+   * If the number of cells is too high,
+   * it decreases the resolution until the number of cells is below a threshold.
+   * */
+  getTileIndices(opts) {
+    let tileRes = Math.max(Math.floor(opts.viewport.zoom / 2 - 1), 0);
+
+    if (
+      typeof opts.minZoom === "number" &&
+      Number.isFinite(opts.minZoom) &&
+      tileRes < opts.minZoom
+    ) {
+      if (!opts.extent) {
+        return [];
+      }
+
+      tileRes = opts.minZoom;
+    }
+
+    if (
+      typeof opts.maxZoom === "number" &&
+      Number.isFinite(opts.maxZoom) &&
+      tileRes > opts.maxZoom
+    ) {
+      tileRes = opts.maxZoom;
+    }
+    const bufferedBounds = makeBufferedBounds(
+      opts.viewport.getBounds(),
+      tileRes,
+    );
+    const cells = fillViewportBBoxes(bufferedBounds, tileRes);
+    return cells.map((h3index) => ({ h3index: h3index }));
+  }
+
+  getTileId({ h3index }) {
+    return h3index;
+  }
+
+  getTileZoom({ h3index }) {
+    return getResolution(h3index);
+  }
+
+  getParentIndex({ h3index }) {
+    const res = getResolution(h3index);
+    // FIXME: this return raises a type warning in the ide which expects an object like
+    //  {x: number, y: number, z: number} and I don't know how to patch this type to {h3index: string}
+    return { h3index: cellToParent(h3index, res - 1) };
+  }
+
+  /** Returns the tile's bounding box
+   * Needed for setting BoundingBox in Tile2DHeader (aka tile param in getTileData of TileLayer)
+   * */
+  getTileMetadata({ h3index }) {
+    const cell_bbox = bbox(lineString(cellToBoundary(h3index, true)));
+    // bbox is [minX, minY, maxX, maxY]
+    return {
+      bbox: {
+        west: cell_bbox[0],
+        north: cell_bbox[3],
+        east: cell_bbox[2],
+        south: cell_bbox[1],
+      },
+    };
+  }
+}
+
+export default class H3TileLayer extends TileLayer {}
+H3TileLayer.defaultProps = {
+  TilesetClass: H3Tileset2D,
+};
--- client/src/containers/grid/map/index.tsx
+++ client/src/containers/grid/map/index.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { useMemo } from "react";
+
 import dynamic from "next/dynamic";
 
+import { DeckLayer } from "@deck.gl/arcgis";
+import { H3HexagonLayer } from "@deck.gl/geo-layers/typed";
+import { ScatterplotLayer } from "@deck.gl/layers/typed";
+import { scaleSequential } from "@visx/vendor/d3-scale";
+import { interpolateViridis } from "d3-scale-chromatic";
+import { cellToLatLng } from "h3-js";
 import { useAtom } from "jotai";
-import { useDebounce, useWindowSize } from "rooks";
+import { useDebounce } from "rooks";
 
 import { tmpBboxAtom, useSyncBbox } from "@/app/store";
 
 import Controls from "@/components/map/controls";
 import BasemapControl from "@/components/map/controls/basemap";
 import ZoomControl from "@/components/map/controls/zoom";
+import Layer from "@/components/map/layers";
+import H3TileLayer from "@/components/map/layers/h3-tile-layer";
 
 const Map = dynamic(() => import("@/components/map"), {
   ssr: false,
@@ -19,11 +29,75 @@ export default function MapContainer() {
   const [bbox, setBbox] = useSyncBbox();
   const [tmpBbox] = useAtom(tmpBboxAtom);
 
-  const { innerWidth } = useWindowSize();
-
   const handleMapMove = useDebounce((extent: __esri.Extent) => {
     setBbox([extent.xmin, extent.ymin, extent.xmax, extent.ymax]);
   }, 500);
+
+  const colorscale = scaleSequential()
+    .domain([0, 5])
+    // .domain([selectedLayer.min_value, selectedLayer.max_value])
+    .interpolator(interpolateViridis);
+
+  const layer = useMemo(() => {
+    return new DeckLayer({
+      "deck.layers": new H3TileLayer({
+        id: "tile-h3s",
+        data: "https://dev.api.amazonia360.dev-vizzuality.com/grid/tile/{h3index}",
+        getTileData: (tile) => {
+          return load(tile.url, ArrowLoader, {
+            arrow: { shape: "object-row-table" },
+          }).then((data) => {
+            return data.data;
+          });
+        },
+        minZoom: 0,
+        maxZoom: 12,
+        maxRequests: 10, // max simultaneous requests. Set 0 for unlimited
+        maxCacheSize: 300, // max number of tiles to keep in the cache
+        renderSubLayers: (props) => {
+          // For zoom < 1 (~whole world view), render a scatterplot layer instead of the hexagon layer
+          // It is faster to render points than hexagons (is it?) when there are many cells.
+          if (props.tile.zoom < 1) {
+            return new ScatterplotLayer({
+              id: props.id,
+              data: props.data,
+              pickable: true,
+              radiusUnits: "meters",
+              getRadius: 9854, // is the radius of a h3 cell at resolution 5 in meters
+              getPosition: (d) =>
+                cellToLatLng(BigInt(d.cell).toString(16)).reverse(),
+              getFillColor: (d) => {
+                const c = color(colorscale(d.fire)).rgb();
+                return [c.r, c.g, c.b];
+              },
+              opacity: 0.8,
+            });
+          }
+          return new H3HexagonLayer({
+            id: props.id,
+            data: props.data,
+            highPrecision: "auto",
+            pickable: true,
+            wireframe: false,
+            filled: true,
+            extruded: false,
+            stroked: false,
+            // getLineColor: [255, 255, 255, 255],
+            // getLineWidth:  10,
+            getHexagon: (d) => {
+              const res = BigInt(d.cell);
+              return res.toString(16);
+            },
+            getFillColor: (d) => {
+              const c = color(colorscale(d.fire)).rgb();
+              return [c.r, c.g, c.b];
+            },
+            opacity: 0.8,
+          });
+        },
+      }),
+    });
+  }, []);
 
   return (
     <div className="w-full flex flex-col grow">
@@ -36,9 +110,11 @@ export default function MapContainer() {
           top: 0,
           right: 0,
           bottom: 0,
-          left: innerWidth ? innerWidth / 2 : 0,
+          left: 0,
         }}
       >
+        <Layer index={0} layer={layer} />
+
         <Controls>
           <ZoomControl />
           <BasemapControl />
