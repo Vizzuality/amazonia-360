332dc9648e38a1903479eba20185f6d202e2ad9e
Biel Stela <biel.stela@gmail.com>
2024-06-20 17:58:22 +0200

    Make use of fastapi tools for auth (#57)
    * Makes use of fastapis tooling to check tokens instead of our own
    
    * Handle token errors in house
    
    * Add comment
    
    * Compact

--- api/app/auth/auth.py
+++ api/app/auth/auth.py
@@ -1,27 +1,15 @@
-"""Auth middleware for the API."""
+"""Auth dependency for the API."""
 
-from fastapi import HTTPException
-from fastapi.security import HTTPBearer
-from starlette.requests import Request
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from app.config.config import get_settings
 
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 
 
-def verify_token(request: Request):
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     """Validate API key."""
-    auth_token = get_settings().auth_token
-    auth_header = request.headers.get("Authorization")
-    if not auth_header:
+    if credentials is None or credentials.credentials != get_settings().auth_token:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized")
-
-    try:
-        scheme, token = auth_header.split()
-        if scheme.lower() != "bearer" or token != auth_token:
-            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized")
-    except ValueError:
-        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid authorization header")  # noqa: B904
-
-    return token
