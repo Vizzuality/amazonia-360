041d2ddd226c42851fd6ee809c3741562d4f2c5f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-12-20 14:40:14 +0100

    Raster: sum

--- client/src/containers/indicators/numeric/imagery.tsx
+++ client/src/containers/indicators/numeric/imagery.tsx
@@ -20,9 +20,28 @@ export const NumericImageryIndicators = ({ id, resource }: NumericImageryIndicat
 
   const VALUE = useMemo(() => {
     if (!query.data || !("statistics" in query.data)) return 0;
+
+    // let sumproducto = 0;
+    // let countPixel = 0;
+    // for (let i=0; i<hs.histograms[0].size; i++) {
+    //     let valor_histo_i = (hs.histograms[0].min + i * hs.histograms[0].max/(hs.histograms[0].size-1))
+    //     console.log(hs.histograms[0].counts[i], "px X", valor_histo_i);
+    //     producto = hs.histograms[0].counts[i] * valor_histo_i;
+    //     sumproducto += producto
+    //     countPixel += hs.histograms[0].counts[i]
+    //     console.log(sumproducto);
+    // }
+    // console.log(sumproducto)
+    // console.log(countPixel)
     // const h = query.data.histograms[0];
-    return query.data.statistics.reduce((acc, curr) => {
-      return acc + (curr.sum ?? 0);
+
+    return query.data.histograms.reduce((acc, curr) => {
+      return (
+        acc +
+        [...curr.counts].reduce((a, c, i) => {
+          return a + c * (curr.min + (i * curr.max) / (curr.size - 1));
+        }, 0)
+      );
     }, 0);
   }, [query.data]);
 
