dcbe699d7ad216da073e3716b3477d860edc09ae
María <maluenarod@gmail.com>
2025-01-02 16:29:45 +0100

    filters limit

--- client/src/containers/report/location/grid/filters/index.tsx
+++ client/src/containers/report/location/grid/filters/index.tsx
@@ -3,15 +3,24 @@
 import { useGetGridMeta } from "@/lib/grid";
 
 import GridFiltersItem from "./item";
+import { useSyncGridDatasets, selectedFiltersViewAtom } from "@/app/store";
+
+import { useAtom } from "jotai";
 
 export default function GridFilters() {
-  const { data: gridMetaData } = useGetGridMeta();
+  const [gridDatasets] = useSyncGridDatasets();
+  const [selectedFiltersView] = useAtom(selectedFiltersViewAtom);
+
+  const { data: gridMetaData } = useGetGridMeta({
+    select: (data) =>
+      selectedFiltersView
+        ? data.datasets.filter((dataset) => gridDatasets.includes(dataset.var_name))
+        : data.datasets,
+  });
 
   return (
     <div className="space-y-1">
-      {gridMetaData?.datasets?.map((dataset) => (
-        <GridFiltersItem key={dataset.var_name} {...dataset} />
-      ))}
+      {gridMetaData?.map((dataset) => <GridFiltersItem key={dataset.var_name} {...dataset} />)}
     </div>
   );
 }
