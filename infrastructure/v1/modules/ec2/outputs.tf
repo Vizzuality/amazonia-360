33e031ae5a9e8add72d633745bce3a0dbb5ab046
alexeh <alexurola@gmail.com>
2025-01-24 07:27:25 +0100

    destroy old infra, reorg tf setup

--- infrastructure/v1/modules/ec2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.amazonia360-ec2.id
-}
--- infrastructure/v1/modules/s3/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "tf-state-bucket-name" {
-  type = string
-  description = "The name of the S3 bucket to store Terraform state"
-}
