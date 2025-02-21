06169a2bbdd82f02cee571749e98eb8fb45ec3e8
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-02-21 10:55:20 +0100

    Chart colors (#126)
    * Report: chart color example
    
    * Report: chart colors depending on the drawingInfo

--- client/src/containers/indicators/chart/index.tsx
+++ client/src/containers/indicators/chart/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 
+import Color from "@arcgis/core/Color";
 import { scaleOrdinal } from "@visx/scale";
 import CHROMA from "chroma-js";
 
@@ -29,13 +30,53 @@ export const ChartIndicators = (indicator: ChartIndicatorsProps) => {
   // console.log("LAYER", queryResourceFeatureLayer.data);
 
   const COLOR_SCALE = useMemo(() => {
+    const range =
+      query.data?.features?.map((d) => {
+        const renderer = queryResourceFeatureLayer.data?.drawingInfo?.renderer;
+
+        if (renderer?.type === "simple") {
+          const r = renderer as __esri.SimpleRenderer;
+          const c = new Color(r.symbol.color);
+          return c.toHex() ?? "#009ADE";
+        }
+
+        if (renderer?.type === "uniqueValue") {
+          const r = renderer as __esri.UniqueValueRenderer;
+
+          const uniqueValue = r.uniqueValueInfos?.find(
+            // @ts-expect-error- I don't know why the type does not correspond to the real data.
+            // It's true that the documentation says that "field1" does not exists https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-UniqueValueRenderer.html#field
+            (u) => `${u.value}` === `${d.attributes[r.field1]}`,
+          );
+
+          if (uniqueValue) {
+            const c = new Color(uniqueValue?.symbol.color);
+            return c.toHex() ?? "#009ADE";
+          }
+        }
+
+        if (renderer?.type === "classBreaks") {
+          const r = renderer as __esri.ClassBreaksRenderer;
+          const classBreak = r.classBreakInfos?.find(
+            // @ts-expect-error- I don't know why the type does not correspond to the real data.
+            // It's true that the documentation says that "classMaxValue" does not exists https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-support-ClassBreakInfo.html
+            (u) => `${u.classMaxValue}` === `${d.attributes[r.field]}`,
+          );
+
+          if (classBreak) {
+            const c = new Color(classBreak?.symbol.color);
+            return c.toHex() ?? "#009ADE";
+          }
+        }
+
+        return "#009ADE";
+      }) ?? [];
+
     return scaleOrdinal({
-      domain: query.data?.features?.map((d) => d.attributes) ?? [],
-      range: CHROMA.scale(["#009ADE", "#93CAEB", "#DBEDF8"]).colors(
-        query.data?.features?.length || 1,
-      ),
+      domain: query.data?.features?.map((d) => d.attributes.label) ?? [],
+      range: range.length ? range : CHROMA.scale("Spectral").colors(10),
     });
-  }, [query.data]);
+  }, [query.data, queryResourceFeatureLayer.data]);
 
   const TOTAL = useMemo(() => {
     if (!query.data) return 0;
