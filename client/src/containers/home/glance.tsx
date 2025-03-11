8c5a5052aa7cf4874ea493438472e313afcf78be
María <maluenarod@gmail.com>
2025-03-11 16:51:15 +0100

    home responsive

--- client/src/containers/home/glance.tsx
+++ client/src/containers/home/glance.tsx
@@ -101,7 +101,7 @@ export default function Glance() {
   return (
     <section
       ref={sectionRef}
-      className="flex flex-col items-end px-4 pt-20 md:container md:flex-row md:space-x-28 md:py-28"
+      className="flex flex-col items-end px-4 pt-20 md:container md:flex-row md:items-start md:space-x-28 md:py-28"
     >
       <div
         className={`flex w-full flex-col space-y-5 md:w-1/2 md:space-y-10 lg:space-y-44 ${isSectionInView ? "overflow-hidden md:duration-700 md:animate-in md:fade-in-0 md:slide-in-from-left-20" : "md:opacity-0"}`}
--- client/src/containers/home/topics/item.tsx
+++ client/src/containers/home/topics/item.tsx
@@ -13,7 +13,7 @@ export default function TopicsItem({ id, name_en, image, description_en }: Topic
   const descriptionRef = useRef<HTMLParagraphElement>(null);
 
   return (
-    <div key={id} className="h-20 w-screen md:h-96 md:w-96">
+    <div key={id} className="h-20 w-screen md:h-96 md:w-64 lg:w-96">
       <div className="group relative cursor-pointer overflow-hidden shadow after:absolute after:bottom-0 after:left-0 after:h-16 after:w-full after:bg-gradient-to-b after:from-transparent after:to-[#09090B]/85 after:content-['']">
         <div className="h-20 w-screen md:h-96 md:w-96">
           <Image
