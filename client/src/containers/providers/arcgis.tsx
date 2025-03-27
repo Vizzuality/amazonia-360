ddb93b41d66a56cf1256f24c382a63df15648065
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-03-27 09:26:10 +0100

    Search: case sensitive (#170)

--- client/src/containers/providers/arcgis.tsx
+++ client/src/containers/providers/arcgis.tsx
@@ -5,6 +5,8 @@ import esriConfig from "@arcgis/core/config";
 
 import { env } from "@/env.mjs";
 
+import { DATASETS } from "@/constants/datasets";
+
 export const ArcGISProvider = ({ children }: PropsWithChildren) => {
   useMemo(() => {
     esriConfig.apiKey = env.NEXT_PUBLIC_ARCGIS_API_KEY;
@@ -13,6 +15,18 @@ export const ArcGISProvider = ({ children }: PropsWithChildren) => {
       urls: [env.NEXT_PUBLIC_API_URL],
       headers: {
         Authorization: `Bearer ${env.NEXT_PUBLIC_API_KEY}`,
+      },
+    });
+
+    esriConfig.request.interceptors?.push({
+      urls: [
+        DATASETS.admin0.layer.url,
+        DATASETS.admin1.layer.url,
+        DATASETS.admin2.layer.url,
+        DATASETS.ciudades_capitales.layer.url,
+      ],
+      query: {
+        collation: JSON.stringify({ accentSensitive: false, caseSensitive: false }),
       },
     });
   }, []);
