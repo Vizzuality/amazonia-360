9f1fbeaf8d780403322ccc008101fa43c842d45a
María Luena Rodríguez <maluenarod@gmail.com>
2025-04-03 11:10:02 +0200

    error and not found page (#174)

--- client/src/app/[locale]/not-found.tsx
+++ client/src/app/[locale]/not-found.tsx
@@ -1,19 +1,18 @@
 import { Button } from "@/components/ui/button";
 
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export default function Custom404() {
+  const t = useTranslations();
   return (
     <div className="text-blue-ocean flex h-[calc(100svh_-_theme(space.40)_+_1px)] w-screen flex-col items-center justify-center bg-white">
       <div className="max-w-[561px] space-y-5 text-center">
-        <h1 className="text-bold text-xl">404 - PAGE NOT FOUND</h1>
-        <p className="text-large">
-          The page you are looking for might have been removed, had its name changed or is
-          temporarily unavailable.
-        </p>
+        <h1 className="text-bold text-xl">{t("not-found-page-title")}</h1>
+        <p className="text-large">{"not-found-page-message"}</p>
         <div>
           <Link href="/">
-            <Button>Go back to home</Button>
+            <Button>{t("go-back-home-button")}</Button>
           </Link>
         </div>
       </div>
