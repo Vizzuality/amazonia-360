caa3e3fd2db8f6244bd0e9eb4eaaa930cf1cd6cb
María <maluenarod@gmail.com>
2025-04-03 10:09:59 +0200

    error and not found page

--- client/src/app/[locale]/error.tsx
+++ client/src/app/[locale]/error.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 export default function Custom500() {
+  const t = useTranslations();
   return (
     <div className="text-blue-ocean flex h-[calc(100svh_-_theme(space.40)_+_1px)] w-screen flex-col items-center justify-center bg-white">
       <div className="max-w-[601px] text-center">
-        <h1 className="text-bold mb-[28px] text-xl">500 - INTERNAL SERVER ERROR</h1>
-        <p className="text-large">
-          The server encountered an internal error or misconfiguration and was unable to complete
-          your request.
-        </p>
+        <h1 className="text-bold mb-[28px] text-xl">{t("error-page-title")}</h1>
+        <p className="text-large">{t("error-page-message")}</p>
       </div>
     </div>
   );
