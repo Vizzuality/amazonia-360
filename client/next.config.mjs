ed53db9133ca89adf5bf8f9022221afc4309fda7
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-04 12:28:22 +0200

    CORS

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -21,22 +21,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "Authorization",
-            value: "Hidden",
-          },
-        ],
+        // matching all API routes
+        source: "/custom-api/:path*",
         headers: [
-          {
-            key: "Authorization",
-            value: "Hidden",
-          },
-        ],
-      },
-    ];
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 };
 
