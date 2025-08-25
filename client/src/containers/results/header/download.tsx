886808e78b39a4e0770cf3aa2b9af2f6d41f35c0
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-08-25 11:27:56 +0200

    Webshot: pdf button

--- client/src/containers/results/header/download.tsx
+++ client/src/containers/results/header/download.tsx
@@ -1,91 +1,50 @@
 "use client";
 
-import {
-  // useMemo,
-  useState,
-} from "react";
+import { useState } from "react";
 
-// import { usePathname, useSearchParams } from "next/navigation";
-
-// import { useAtomValue } from "jotai";
 import { Download } from "lucide-react";
+import { useLocale } from "next-intl";
 
-// import { generatedAITextAtom } from "@/app/store";
-import {
-  // GeoJSONPolygon,
-  filenameFromCD,
-  downloadBlobResponse,
-} from "@/lib/pdf";
+import { usePostWebshotReportMutation } from "@/lib/webshot";
 
 import { Button } from "@/components/ui/button";
 
-// type PDFPayload = {
-//   pagePath: string;
-//   geometry?: GeoJSONPolygon;
-//   generatedTextContent?: object;
-// };
-
 export default function ReportButton() {
   const [status, setStatus] = useState<"idle" | "pending">("idle");
-  // const generatedTextContent = useAtomValue(generatedAITextAtom);
-  // const pathname = usePathname();
-  // const URLparams = useSearchParams()?.toString();
-  // const bbox = useSearchParams()?.get("?bbox") ?? undefined;
-  // const isAIEnabled = useSearchParams()?.get("aiSummary") ?? undefined;
+  const locale = useLocale();
 
-  // const geometry = useMemo(() => {
-  //   if (!bbox) return undefined;
-  //   try {
-  //     const bboxParsed = parseBBox(bbox);
-  //     return bboxToGeoJSONPolygon(bboxParsed);
-  //   } catch {
-  //     return undefined;
-  //   }
-  // }, [bbox]);
+  const postWebshotReportMutation = usePostWebshotReportMutation();
 
-  // const pagePath = useMemo(() => `${pathname}?${URLparams}`, [pathname, URLparams]);
-
-  const handleClick = async () => {
-    // const payload: PDFPayload = { pagePath };
-    // if (geometry) payload.geometry = geometry;
-    // if (isAIEnabled) payload.generatedTextContent = generatedTextContent;
-    const fakePayload = {
-      pagePath:
-        '/en/report/results?%3Fbbox=-9179719.906994147,-723616.366654413,-7381921.00172728,109241.49354064604&topics={"id":1,"indicators":[{"id":26,"type":"numeric","x":3,"y":0,"w":1,"h":1},{"id":26,"type":"map","x":2,"y":3,"w":2,"h":4},{"id":10,"type":"chart","x":2,"y":1,"w":2,"h":2},{"id":13,"type":"numeric","x":0,"y":0,"w":1,"h":1},{"id":25,"type":"numeric","x":2,"y":0,"w":1,"h":1},{"id":12,"type":"numeric","x":1,"y":0,"w":1,"h":1},{"id":29,"type":"map","x":0,"y":5,"w":2,"h":4},{"id":24,"type":"chart","x":0,"y":1,"w":2,"h":2}]},{"id":4,"indicators":[{"id":72,"type":"map","x":0,"y":0,"w":2,"h":4},{"id":58,"type":"map","x":2,"y":0,"w":2,"h":4}]}&location={"type":"polygon","geometry":{"spatialReference":{"wkid":102100},"rings":[[[-8280820.454360714,-260331.5382180824],[-7778132.337843387,19848.38896115661],[-7925655.802433738,-634223.2620749235],[-8280820.454360714,-260331.5382180824]]]},"buffer":0}&gridFiltersSetUp={"limit":10,"opacity":100,"direction":"desc"}',
-    };
+  const handleClick = () => {
     setStatus("pending");
-    try {
-      const res = await fetch("/local-api/report", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(fakePayload),
-      });
 
-      if (!res.ok) {
-        const err = await res.json().catch(() => ({}));
-        return new Response(
-          JSON.stringify({
-            error: err.message || err.error || `Error ${res.status}`,
-          }),
-          {
-            status: res.status,
-            headers: { "Content-Type": "application/json" },
-          },
-        );
-      }
+    postWebshotReportMutation.mutate(
+      {
+        pagePath: `/${locale}/webshot/pdf`,
+        generatedTextContent: {
+          2: "Dynamic text content for section 2",
+          5: "Dynamic text content for section 5",
+        }, // Optional: Add any dynamic text content here
+      },
+      {
+        onSettled: () => {
+          setStatus("idle");
+        },
+        onError: (error) => {
+          console.error("Error generating Report:", error);
+          setStatus("idle");
+        },
+        onSuccess: async (data) => {
+          console.info(data);
+          // const contentDisposition = data.headers["content-disposition"];
+          // const filename = filenameFromCD(contentDisposition) || "report.pdf";
 
-      const filename = filenameFromCD(res.headers.get("Content-Disposition"));
-
-      await downloadBlobResponse(res, filename);
-    } catch (error) {
-      console.error("Error generating PDF:", error);
-      return new Response(JSON.stringify({ error: "Failed to generate PDF" }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
-    } finally {
-      setStatus("idle");
-    }
+          // if (data?.data) {
+          //   await downloadBlobResponse(data?.data, filename);
+          // }
+        },
+      },
+    );
   };
 
   return (
@@ -95,7 +54,7 @@ export default function ReportButton() {
       onClick={handleClick}
       disabled={status === "pending"}
     >
-      {status === "pending" ? <span>Generating PDF...</span> : <Download className="h-5 w-5" />}
+      {status === "pending" ? <span>Generating Report...</span> : <Download className="h-5 w-5" />}
     </Button>
   );
 }
--- client/src/lib/webshot.ts
+++ client/src/lib/webshot.ts
@@ -4,7 +4,89 @@ import axios from "axios";
 /**
  ************************************************************
  ************************************************************
- * Search
+ * WebshotReport
+ * usePostWebshotReportMutation
+ ************************************************************
+ ************************************************************
+ */
+
+export type PostWebshotReportParams = {
+  pagePath: string;
+  generatedTextContent?: Record<string, unknown>;
+};
+
+export const postWebshotReport = async (payload: PostWebshotReportParams) => {
+  return axios.request<Blob>({
+    url: "/local-api/webshot/report",
+    method: "POST",
+    responseType: "blob",
+    headers: { "Content-Type": "application/json" },
+    data: payload,
+  });
+};
+
+export const postWebshotReportMutationOptions = <TError = unknown, TContext = unknown>(
+  options?: UseMutationOptions<
+    Awaited<ReturnType<typeof postWebshotReport>>,
+    TError,
+    PostWebshotReportParams,
+    TContext
+  >,
+): UseMutationOptions<
+  Awaited<ReturnType<typeof postWebshotReport>>,
+  TError,
+  PostWebshotReportParams,
+  TContext
+> => {
+  const mutationFn: MutationFunction<
+    Awaited<ReturnType<typeof postWebshotReport>>,
+    PostWebshotReportParams
+  > = (vars) => postWebshotReport(vars);
+  return { mutationFn, ...options };
+};
+
+export const usePostWebshotReportMutation = <TError = unknown, TContext = unknown>(
+  options?: UseMutationOptions<
+    Awaited<ReturnType<typeof postWebshotReport>>,
+    TError,
+    PostWebshotReportParams,
+    TContext
+  >,
+) => {
+  return useMutation(postWebshotReportMutationOptions(options));
+};
+
+export async function downloadBlobResponse(res: Response, fallbackName = "report.pdf") {
+  if (!res) return;
+
+  const cd = res.headers.get("Content-Disposition") ?? undefined;
+  const match = cd?.match(/filename\*?=.*?''?([^;]+)|filename="?([^"]+)"?/i);
+  const filename = match?.[1] || match?.[2] || fallbackName;
+
+  const blob = await res.blob();
+
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = decodeURIComponent(filename.trim());
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
+const date = new Date().toISOString().split("T")[0];
+
+export function filenameFromCD(cd?: string | null, fallback = `report_${date}.pdf`) {
+  if (!cd) return fallback;
+  const m = /filename\*?=.*?''?([^;]+)|filename="?([^"]+)"?/i.exec(cd);
+  return decodeURIComponent((m?.[1] || m?.[2] || fallback).trim());
+}
+
+/**
+ ************************************************************
+ ************************************************************
+ * WebshotWidgets
  * usePostWebshotWidgetsMutation
  ************************************************************
  ************************************************************
