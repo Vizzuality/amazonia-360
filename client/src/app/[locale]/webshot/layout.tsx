28ed79f2f1c2297c5bd63a036aafdac34072fdde
Carlos Díaz <linkmetal18@gmail.com>
2025-09-04 11:12:01 +0100

    feat: restructure layout components and split them to allow webshot use a diferent one

---
+++ client/src/app/[locale]/webshot/layout.tsx
@@ -0,0 +1,56 @@
+import { Suspense } from "react";
+
+import { notFound } from "next/navigation";
+
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+
+import { routing } from "@/i18n/routing";
+
+import "@arcgis/core/assets/esri/themes/light/main.css";
+import "@/styles/globals.css";
+import "@/styles/grid-layout.css";
+import "react-resizable/css/styles.css";
+import { Montserrat } from "next/font/google";
+import RootHead from "@/app/head";
+
+type Params = Promise<{ locale: string }>;
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({
+    locale,
+  }));
+}
+
+const montserrat = Montserrat({
+  weight: ["500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--montserrat",
+});
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Params;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  return (
+    <>
+      <RootHead />
+      <body className={`${montserrat.className}`}>
+        <Suspense fallback={null}>
+          <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>
+        </Suspense>
+      </body>
+    </>
+  );
+}
