466f934b14acaa8d35165a21ddf2af3ae9e3b8e5
Biel Stela <biel.stela@gmail.com>
2025-09-05 10:20:06 +0200

    Remove all tiler related bits in api (#218)

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,20 +1,17 @@
 from fastapi.routing import APIRoute
 
-from tests.conftest import FILES, HEADERS
+from tests.conftest import HEADERS
 from tests.utils import test_client
 
 
 def test_no_token_is_unauthorized():
-    response = test_client.get("/tifs")
-    response2 = test_client.post("/exact_zonal_stats")
+    response = test_client.get("/grid/meta")
     assert response.status_code == 401
-    assert response2.status_code == 401
     assert response.json() == {"detail": "Unauthorized"}
-    assert response2.json() == {"detail": "Unauthorized"}
 
 
 def test_wrong_token_is_unauthorized():
-    response = test_client.get("/tifs", headers={"Authorization": "Bearer BAD-TOKKI-123"})
+    response = test_client.get("/grid/meta", headers={"Authorization": "Bearer BAD-TOKKI-123"})
     assert response.status_code == 401
     assert response.json() == {"detail": "Unauthorized"}
 
@@ -27,29 +24,15 @@ def test_health_is_public():
 
 def test_options_request_is_allowed_with_correct_headers():
     headers = {"Access-Control-Request-Method": "GET", "Origin": "http://example.com"}
-    response = test_client.options("/tifs", headers=headers)
-    response2 = test_client.options("/exact_zonal_stats", headers=headers)
+    response = test_client.options("/grid/meta", headers=headers)
     assert response.status_code == 200
-    assert response2.status_code == 200
     assert "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT" in response.headers["Access-Control-Allow-Methods"]
     assert "*" in response.headers["Access-Control-Allow-Origin"]
 
 
-def test_with_token(setup_data_folder):
-    response = test_client.get("/tifs", headers=HEADERS)
+def test_request_with_token(setup_data_folder):
+    response = test_client.get("/grid/meta", headers=HEADERS)
     assert response.status_code == 200
-
-
-def test_list_files_empty(setup_data_folder):
-    response = test_client.get("/tifs", headers=HEADERS)
-    assert response.status_code == 200
-    assert response.json() == {"files": []}
-
-
-def test_list_files(setup_empty_files):
-    response = test_client.get("/tifs", headers=HEADERS)
-    assert response.status_code == 200
-    assert response.json() == {"files": FILES}
 
 
 def test_all_api_routes_require_token():
