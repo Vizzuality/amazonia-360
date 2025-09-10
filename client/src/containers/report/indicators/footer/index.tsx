7297977e25e2d754577c83afac02a5b2b22a820d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-09-10 18:29:33 +0200

    Indicators: controls expand all

--- client/src/containers/report/indicators/footer/index.tsx
+++ client/src/containers/report/indicators/footer/index.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import { useTranslations } from "next-intl";
+import { useCallback } from "react";
 
-import { useSyncIndicators } from "@/app/store";
+import { useSetAtom } from "jotai";
+import { useLocale, useTranslations } from "next-intl";
+
+import { useGetDefaultTopics } from "@/lib/topics";
+
+import { indicatorsExpandAtom, useSyncIndicators } from "@/app/store";
 
 import { Button } from "@/components/ui/button";
 
 export default function IndicatorsFooter() {
   const t = useTranslations();
+  const locale = useLocale();
   const [indicators, setIndicators] = useSyncIndicators();
+  const setIndicatorsExpand = useSetAtom(indicatorsExpandAtom);
+
+  const { data: topicsData } = useGetDefaultTopics({ locale });
+
+  const handleExpandAll = useCallback(() => {
+    setIndicatorsExpand(() => {
+      return topicsData?.reduce(
+        (acc, topic) => {
+          acc[topic.id] = [];
+          return acc;
+        },
+        {} as Record<number, number[]>,
+      );
+    });
+  }, [topicsData, setIndicatorsExpand]);
 
   const handleClear = () => {
     setIndicators(null);
@@ -16,7 +37,7 @@ export default function IndicatorsFooter() {
 
   return (
     <div className="flex items-center justify-between">
-      <Button variant="ghost" size="sm" className="invisible">
+      <Button variant="ghost" size="sm" onClick={handleExpandAll}>
         Expand all
       </Button>
 
--- client/src/containers/report/indicators/topics/item.tsx
+++ client/src/containers/report/indicators/topics/item.tsx
@@ -4,13 +4,14 @@ import { useMemo } from "react";
 
 import Image from "next/image";
 
+import { useAtom } from "jotai";
 import { useLocale } from "next-intl";
 
 import { PLACEHOLDER } from "@/lib/images";
 import { useGetDefaultIndicators } from "@/lib/indicators";
 import { cn } from "@/lib/utils";
 
-import { useSyncIndicators } from "@/app/store";
+import { indicatorsExpandAtom, useSyncIndicators } from "@/app/store";
 
 import { Topic } from "@/constants/topics";
 
@@ -25,11 +26,28 @@ export default function TopicsItem({ id, name, image }: TopicsItemProps) {
   const locale = useLocale();
   const { data: indicatorsData } = useGetDefaultIndicators(id, locale);
   const [indicators] = useSyncIndicators();
+  const [indicatorsExpand, setIndicatorsExpand] = useAtom(indicatorsExpandAtom);
 
   const SELECTED = useMemo(() => {
     if (!indicatorsData || !indicators) return 0;
     return indicatorsData.filter((indicator) => indicators.includes(indicator.id)).length;
   }, [indicatorsData, indicators]);
+
+  const handleClick = (open: boolean) => {
+    setIndicatorsExpand((prev) => {
+      if (open) {
+        return {
+          ...prev,
+          [id]: [],
+        };
+      } else {
+        return {
+          ...prev,
+          [id]: undefined,
+        };
+      }
+    });
+  };
 
   return (
     <div
@@ -38,7 +56,7 @@ export default function TopicsItem({ id, name, image }: TopicsItemProps) {
         "h-full w-full grow cursor-pointer overflow-hidden rounded-sm bg-white text-left",
       )}
     >
-      <Collapsible>
+      <Collapsible open={!!indicatorsExpand?.[id]} onOpenChange={handleClick}>
         <CollapsibleTrigger
           className={cn(
             "flex w-full items-center justify-between space-x-2.5 p-1 transition-colors duration-300 ease-in-out hover:bg-blue-50",
