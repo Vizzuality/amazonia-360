42d3fd7d7920d04ff768feb791b0415e11d35a28
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-16 09:51:46 +0200

    PDF: header tooltips

--- client/src/containers/header/results/download.tsx
+++ client/src/containers/header/results/download.tsx
@@ -7,15 +7,34 @@ import { FileDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import { Link } from "@/i18n/navigation";
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { useTranslations } from "next-intl";
+import { TooltipPortal } from "@radix-ui/react-tooltip";
 
 export default function ReportButton() {
+  const t = useTranslations();
   const searchParams = useSearchParams();
 
   return (
-    <Link href={`/webshot/report/?${searchParams.toString()}`} target="_blank">
-      <Button variant="outline" className="space-x-2 border-none px-2.5 py-2 shadow-none">
-        <FileDown className="h-5 w-5" />
-      </Button>
-    </Link>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Link
+          href={`/webshot/report/?${searchParams.toString()}`}
+          target="_blank"
+          className="block"
+        >
+          <Button variant="outline" className="space-x-2 border-none px-2.5 py-2 shadow-none">
+            <FileDown className="h-5 w-5" />
+          </Button>
+        </Link>
+      </TooltipTrigger>
+
+      <TooltipPortal>
+        <TooltipContent side="bottom">
+          <span>{t("download")}</span>
+          <TooltipArrow />
+        </TooltipContent>
+      </TooltipPortal>
+    </Tooltip>
   );
 }
