01115354ac648fdc59254789c60802d062c61ba5
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-20 00:24:16 +0200

    PDF: generating report strings

--- client/src/containers/webshot/pdf-report/index.tsx
+++ client/src/containers/webshot/pdf-report/index.tsx
@@ -2,6 +2,8 @@
 
 import { useRef, useState } from "react";
 
+import { useTranslations } from "next-intl";
+
 import { cn } from "@/lib/utils";
 
 import { LoadProvider } from "@/containers/indicators/load-provider";
@@ -12,6 +14,8 @@ import PfdGeographicContext from "@/containers/webshot/pdf-report/geographic-con
 import PdfTopics from "@/containers/webshot/pdf-report/topics";
 
 export const Pdf = () => {
+  const t = useTranslations();
+
   const ref = useRef<HTMLDivElement>(null);
   const [loaded, setLoaded] = useState(false);
 
@@ -22,8 +26,11 @@ export const Pdf = () => {
   return (
     <LoadProvider onLoad={handleLoad}>
       {!loaded && (
-        <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-foreground/80 text-background">
-          <p className="text-xl font-bold">Generating report...</p>
+        <div className="fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-foreground/90 text-background">
+          <div className="flex max-w-sm flex-col items-center justify-center gap-2 text-center">
+            <h3 className="text-xl font-bold">{t("pdf-report-generating")}</h3>
+            <p className="text-sm">{t("pdf-report-generating-subtitle")}</p>
+          </div>
         </div>
       )}
       <div
