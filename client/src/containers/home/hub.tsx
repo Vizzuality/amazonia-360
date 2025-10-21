0fa68844519b1ed62281967a3a6a8a28c0b95687
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-21 12:37:59 +0200

    Home: cta section

--- client/src/containers/home/hub.tsx
+++ client/src/containers/home/hub.tsx
@@ -17,11 +17,11 @@ export default function Hub() {
     >
       <div className="container">
         <div className="flex max-w-xl flex-col items-start justify-start space-y-8">
-          <h3 className="text-2xl font-bold">{t("landing-hub-key")}</h3>
-          <p className="max-w-lg font-normal">{t("landing-hub-title")}</p>
-          <Link href="/report/indicators">
+          <h3 className="text-2xl font-bold">{t("landing-cta-key")}</h3>
+          <p className="max-w-lg font-normal">{t("landing-cta-title")}</p>
+          <Link href="/report">
             <Button size="lg" variant="secondary" className="flex space-x-2.5">
-              <p> {t("landing-hub-access-hub")}</p>
+              <p> {t("landing-cta-access")}</p>
               <LuArrowRight size={20} strokeWidth={1} />
             </Button>
           </Link>
