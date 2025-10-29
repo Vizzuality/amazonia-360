bf0046d1c0a7b230c36777beb082d356bf65211a
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-29 13:21:05 +0100

    Auth: sign in and sign up

---
+++ client/src/app/(frontend)/[locale]/(app)/auth/sign-in/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+
+import { Locale } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+
+type Params = Promise<{ locale: Locale }>;
+
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
+
+  return {
+    title: t("metadata-signin-page-title"),
+    description: t("metadata-signin-page-description"),
+  };
+}
+
+export default async function SignInPage({ params }: { params: Params }) {
+  const { locale } = await params;
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  return <>Sign In Page - locale: {locale}</>;
+}
