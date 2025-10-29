b4600f792b62b83e3498c0ad08490e9fb78fc960
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-29 11:15:34 +0100

    CMS: users roles

--- client/src/cms/collections/Users.ts
+++ client/src/cms/collections/Users.ts
@@ -1,13 +1,46 @@
 import type { CollectionConfig } from "payload";
 
+import { adminAccess, fieldAdminAccess } from "@/cms/access/admin";
+import { anyoneAccess } from "@/cms/access/anyone";
+import { userAccess } from "@/cms/access/user";
+import { protectRole } from "@/cms/hooks/auth";
+
 export const Users: CollectionConfig = {
   slug: "users",
   admin: {
     useAsTitle: "email",
   },
   auth: true,
+  access: {
+    create: anyoneAccess,
+    read: userAccess,
+    update: userAccess,
+    delete: adminAccess,
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: "role",
+      type: "select",
+      required: true,
+      defaultValue: "user",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "User",
+          value: "user",
+        },
+      ],
+      access: {
+        update: fieldAdminAccess,
+      },
+      hooks: {
+        beforeChange: [protectRole],
+      },
+    },
   ],
 };
--- client/src/payload-types.ts
+++ client/src/payload-types.ts
@@ -69,6 +69,7 @@ export interface Config {
   collections: {
     users: User;
     media: Media;
+    reports: Report;
     'payload-locked-documents': PayloadLockedDocument;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
@@ -77,12 +78,13 @@ export interface Config {
   collectionsSelect: {
     users: UsersSelect<false> | UsersSelect<true>;
     media: MediaSelect<false> | MediaSelect<true>;
+    reports: ReportsSelect<false> | ReportsSelect<true>;
     'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
     'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
     'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
   };
   db: {
-    defaultIDType: string;
+    defaultIDType: number;
   };
   globals: {};
   globalsSelect: {};
@@ -118,7 +120,8 @@ export interface UserAuthOperations {
  * via the `definition` "users".
  */
 export interface User {
-  id: string;
+  id: number;
+  role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -142,7 +145,7 @@ export interface User {
  * via the `definition` "media".
  */
 export interface Media {
-  id: string;
+  id: number;
   alt: string;
   updatedAt: string;
   createdAt: string;
@@ -158,23 +161,111 @@ export interface Media {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "reports".
+ */
+export interface Report {
+  id: number;
+  title?: string | null;
+  description?: {
+    root: {
+      type: string;
+      children: {
+        type: any;
+        version: number;
+        [k: string]: unknown;
+      }[];
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+      indent: number;
+      version: number;
+    };
+    [k: string]: unknown;
+  } | null;
+  location:
+    | {
+        type: 'search';
+        key: string | number;
+        text: string;
+        sourceIndex: number;
+      }
+    | {
+        type: 'point' | 'multipoint' | 'polyline' | 'polygon' | 'multipatch' | 'extent';
+        geometry: {
+          [k: string]: unknown;
+        };
+        buffer: number;
+      };
+  topics?:
+    | {
+        topic_id?: number | null;
+        description?: {
+          root: {
+            type: string;
+            children: {
+              type: any;
+              version: number;
+              [k: string]: unknown;
+            }[];
+            direction: ('ltr' | 'rtl') | null;
+            format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+            indent: number;
+            version: number;
+          };
+          [k: string]: unknown;
+        } | null;
+        indicators?:
+          | {
+              indicator_id?: number | null;
+              type?: ('map' | 'chart' | 'table' | 'numeric' | 'custom') | null;
+              x?: number | null;
+              y?: number | null;
+              w?: number | null;
+              h?: number | null;
+              basemapId?:
+                | (
+                    | 'gray-vector'
+                    | 'dark-gray-vector'
+                    | 'satellite'
+                    | 'streets'
+                    | 'hybrid'
+                    | 'osm'
+                    | 'topo-vector'
+                    | 'terrain'
+                  )
+                | null;
+              opacity?: number | null;
+              id?: string | null;
+            }[]
+          | null;
+        id?: string | null;
+      }[]
+    | null;
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
-  id: string;
+  id: number;
   document?:
     | ({
         relationTo: 'users';
-        value: string | User;
+        value: number | User;
       } | null)
     | ({
         relationTo: 'media';
-        value: string | Media;
+        value: number | Media;
+      } | null)
+    | ({
+        relationTo: 'reports';
+        value: number | Report;
       } | null);
   globalSlug?: string | null;
   user: {
     relationTo: 'users';
-    value: string | User;
+    value: number | User;
   };
   updatedAt: string;
   createdAt: string;
@@ -184,10 +275,10 @@ export interface PayloadLockedDocument {
  * via the `definition` "payload-preferences".
  */
 export interface PayloadPreference {
-  id: string;
+  id: number;
   user: {
     relationTo: 'users';
-    value: string | User;
+    value: number | User;
   };
   key?: string | null;
   value?:
@@ -207,7 +298,7 @@ export interface PayloadPreference {
  * via the `definition` "payload-migrations".
  */
 export interface PayloadMigration {
-  id: string;
+  id: number;
   name?: string | null;
   batch?: number | null;
   updatedAt: string;
@@ -218,6 +309,7 @@ export interface PayloadMigration {
  * via the `definition` "users_select".
  */
 export interface UsersSelect<T extends boolean = true> {
+  role?: T;
   updatedAt?: T;
   createdAt?: T;
   email?: T;
@@ -252,6 +344,37 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "reports_select".
+ */
+export interface ReportsSelect<T extends boolean = true> {
+  title?: T;
+  description?: T;
+  location?: T;
+  topics?:
+    | T
+    | {
+        topic_id?: T;
+        description?: T;
+        indicators?:
+          | T
+          | {
+              indicator_id?: T;
+              type?: T;
+              x?: T;
+              y?: T;
+              w?: T;
+              h?: T;
+              basemapId?: T;
+              opacity?: T;
+              id?: T;
+            };
+        id?: T;
+      };
+  updatedAt?: T;
+  createdAt?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
