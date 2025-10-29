b69b539f43bc5142698f263a4f19b72f1b1cdcc3
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-29 13:45:38 +0100

    Auth: sign in and sign up forms. Not connected

--- client/src/app/(frontend)/[locale]/(app)/auth/sign-in/page.tsx
+++ client/src/app/(frontend)/[locale]/(app)/auth/sign-in/page.tsx
@@ -3,6 +3,8 @@ import { Metadata } from "next";
 import { Locale } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
+import { SignInForm } from "@/containers/auth/sign-in";
+
 type Params = Promise<{ locale: Locale }>;
 
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
@@ -21,5 +23,11 @@ export default async function SignInPage({ params }: { params: Params }) {
   // Enable static rendering
   setRequestLocale(locale);
 
-  return <>Sign In Page - locale: {locale}</>;
+  return (
+    <section className="flex grow items-center justify-center">
+      <div className="mx-auto max-w-sm">
+        <SignInForm />
+      </div>
+    </section>
+  );
 }
