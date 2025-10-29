3937f584fa4c24441b1e59eb7de7aafbe48b3c08
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-10-29 11:15:34 +0100

    CMS: users roles

--- client/src/payload.config.ts
+++ client/src/payload.config.ts
@@ -17,6 +17,9 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
+  graphQL: {
+    disable: true,
+  },
   admin: {
     user: Users.slug,
     importMap: {
