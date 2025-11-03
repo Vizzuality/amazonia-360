ea8ebecf254e6603e6e03419cca04c6e4f51f0d8
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-11-03 11:16:19 +0100

    Absolute path

--- client/src/lib/url.ts
+++ client/src/lib/url.ts
@@ -1,9 +1,16 @@
 import { env } from "@/env.mjs";
 
+let absoluteUrl = "";
+
 export const getAbsoluteURL = (path: string) => {
-  if (process.env.VERCEL_BRANCH_URL) {
-    return `https://${process.env.VERCEL_BRANCH_URL}${path}`;
+  if (absoluteUrl) {
+    return absoluteUrl;
   }
 
-  return `${env.NEXT_PUBLIC_URL || "http://localhost:3000"}${path}`;
+  if (process.env.VERCEL_BRANCH_URL) {
+    absoluteUrl = `https://${process.env.VERCEL_BRANCH_URL}${path}`;
+  }
+
+  absoluteUrl = `${env.NEXT_PUBLIC_URL || "http://localhost:3000"}${path}`;
+  return absoluteUrl;
 };
