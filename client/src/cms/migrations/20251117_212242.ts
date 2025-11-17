0fb9104e87a6314beac42d1a6d8fe3f3529ab446
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-11-17 22:24:37 +0100

    Auth: verify email

---
+++ client/src/cms/migrations/20251117_212242.ts
@@ -0,0 +1,13 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "users" ADD COLUMN "_verified" boolean;
+  ALTER TABLE "users" ADD COLUMN "_verificationtoken" varchar;`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "users" DROP COLUMN "_verified";
+  ALTER TABLE "users" DROP COLUMN "_verificationtoken";`)
+}
---
+++ client/src/containers/auth/verify-email.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { Link } from "@/i18n/navigation";
+
+export function VerifyEmail(props: React.ComponentProps<"div">) {
+  const t = useTranslations();
+
+  return (
+    <Card className="border-none shadow-none" {...props}>
+      <CardHeader>
+        <CardTitle className="text-3xl text-primary">{t("auth-verify-email-title")}</CardTitle>
+        <CardDescription className="font-medium">
+          {t("auth-verify-email-description")}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Link href="/auth/sign-in">
+          <Button size="lg" className="w-full">
+            {t("auth-link-back-to-sign-in")}
+          </Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
