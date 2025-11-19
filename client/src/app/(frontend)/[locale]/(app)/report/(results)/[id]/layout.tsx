a5652f7afab61e9ed95644fd9898b42fc21251a5
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-11-19 09:14:33 +0100

    Report: new urls

---
+++ client/src/app/(frontend)/[locale]/(app)/report/(results)/[id]/layout.tsx
@@ -0,0 +1,3 @@
+export default async function ReportNewLayout({ children }: LayoutProps<"/[locale]/report/[id]">) {
+  return <>{children}</>;
+}
--- client/src/app/(frontend)/[locale]/(app)/report/(results)/[id]/page.tsx
+++ client/src/app/(frontend)/[locale]/(app)/report/(results)/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { Metadata } from "next";
 
+import { notFound } from "next/navigation";
+
+import { getPayload } from "payload";
+
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 import { Locale } from "next-intl";
 import { getTranslations } from "next-intl/server";
@@ -9,6 +13,8 @@ import { reportQueryOptions } from "@/lib/report";
 import DataDisclaimer from "@/containers/disclaimers/data";
 import { LoadProvider } from "@/containers/indicators/load-provider";
 import { ReportResults } from "@/containers/results";
+
+import config from "@/payload.config";
 
 type Params = Promise<{ locale: Locale }>;
 
@@ -22,12 +28,20 @@ export async function generateMetadata({ params }: { params: Params }): Promise<
   };
 }
 
-export default async function ReportResultsPage({
-  params,
-}: PageProps<"/[locale]/report/results/[id]">) {
+export default async function ReportResultsPage({ params }: PageProps<"/[locale]/report/[id]">) {
   const queryClient = new QueryClient();
 
   const { id } = await params;
+
+  const payload = await getPayload({ config });
+  try {
+    await payload.findByID({
+      collection: "reports",
+      id: Number(id),
+    });
+  } catch (_error) {
+    notFound();
+  }
 
   await queryClient.prefetchQuery(reportQueryOptions({ id: Number(id) }));
 
