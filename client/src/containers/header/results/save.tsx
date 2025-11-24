2edf27fde4dfdca226891975fd0beae6ad79ea3c
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-11-24 11:35:15 +0100

    Report: jotai atoms improvement

--- client/src/containers/header/results/save.tsx
+++ client/src/containers/header/results/save.tsx
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 import { useParams } from "next/navigation";
 
 import { useSession } from "next-auth/react";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { LuFileText } from "react-icons/lu";
 import { toast } from "sonner";
 
@@ -15,6 +15,7 @@ import { Button } from "@/components/ui/button";
 
 export default function SaveReport() {
   const locale = useLocale();
+  const t = useTranslations();
   const { id } = useParams();
 
   const { data: session } = useSession();
@@ -29,7 +30,7 @@ export default function SaveReport() {
     toast.promise(
       saveMutation.mutateAsync({
         id: Number(id),
-        title: title || "Untitled Report",
+        title: title || t("selected-area"),
         topics: topics || [],
         location: location,
         locale,
@@ -41,7 +42,7 @@ export default function SaveReport() {
         error: "Failed to save the report.",
       },
     );
-  }, [id, title, topics, location, locale, session, saveMutation]);
+  }, [id, title, topics, location, locale, session, saveMutation, t]);
 
   return (
     <Button onClick={handleSave} className="space-x-2">
