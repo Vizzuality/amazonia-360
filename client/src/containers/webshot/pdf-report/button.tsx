ebc6526be390444268d262ba7f0ea5bade474452
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-12-16 09:37:02 +0100

    PDF: print button (#296)

--- client/src/containers/webshot/pdf-report/button.tsx
+++ client/src/containers/webshot/pdf-report/button.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
-export const PrintButton = ({ onClick }: { onClick: () => void }) => {
+import { Button } from "@/components/ui/button";
+import { Markdown } from "@/components/ui/markdown";
+
+export const PrintButton = () => {
+  const t = useTranslations();
+
   return (
-    <Button
-      className="fixed bottom-4 right-4 z-50 print:hidden"
-      variant="secondary"
-      size="lg"
-      onClick={onClick}
-    >
-      Print
-    </Button>
+    <header className="bg-muted py-5 print:hidden">
+      <div className="relative mx-auto flex w-[11in] items-center justify-between space-x-2.5">
+        <div className="space-y-2">
+          <h1 className="text-base font-semibold text-foreground">
+            {t("pdf-report-button-title")}
+          </h1>
+          <Markdown>{t("pdf-report-button-description")}</Markdown>
+        </div>
+        <Button
+          variant="default"
+          size="lg"
+          onClick={() => {
+            window.print();
+          }}
+        >
+          {t("pdf-report-button-text")}
+        </Button>
+      </div>
+    </header>
   );
 };
 
