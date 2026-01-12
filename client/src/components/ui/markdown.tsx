047c22c1af3fdc2d6ac8da91d44009d448e7bd26
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2026-01-12 09:53:40 +0100

    Underline: markdown support

--- client/src/components/ui/markdown.tsx
+++ client/src/components/ui/markdown.tsx
@@ -1,11 +1,14 @@
 import ReactMarkdown from "react-markdown";
 
+import rehypeRaw from "rehype-raw";
+
 import { cn } from "@/lib/utils";
 
 export const Markdown = ({ children, className }: { children?: string; className?: string }) => {
   return (
     <ReactMarkdown
       className={cn("prose prose-sm font-medium prose-a:break-words", className)}
+      rehypePlugins={[rehypeRaw]}
       components={{
         a: (props) => (
           <a href={props.href} target="_blank" rel="noopener noreferrer">
