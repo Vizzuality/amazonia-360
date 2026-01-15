388339a5269b11fec874d74088565cd154af7747
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2026-01-15 11:53:20 +0100

    Fix switch background issue (#302)

--- client/src/components/ui/switch.tsx
+++ client/src/components/ui/switch.tsx
@@ -12,7 +12,7 @@ const SwitchThumb = React.forwardRef<
 >(() => (
   <SwitchPrimitives.Thumb
     className={cn(
-      "pointer-events-none block h-3 w-3 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+      "pointer-events-none z-10 block h-3 w-3 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
     )}
   />
 ));
@@ -24,12 +24,13 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "peer relative inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-blue-100",
       className,
     )}
     {...props}
     ref={ref}
   >
+    <span className="absolute -left-0.5 -top-0.5 h-[calc(100%+4px)] w-[calc(100%+4px)] rounded-full bg-black/10"></span>
     <SwitchThumb />
   </SwitchPrimitives.Root>
 ));
