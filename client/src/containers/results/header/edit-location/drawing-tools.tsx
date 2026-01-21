96d24a79ab783123edfd40e00cc50b3898f2cb3d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2026-01-21 11:29:41 +0100

    Report: edit location drawing tools (#305)

---
+++ client/src/containers/results/header/edit-location/drawing-tools.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+
+import SketchButtons from "@/components/map/sketch/sketch-buttons";
+
+export default function EditLocationDrawingTools() {
+  const t = useTranslations();
+
+  return (
+    <div className="flex items-center space-x-4 print:hidden">
+      <span className="text-sm font-semibold text-muted-foreground">{t("draw")}</span>
+      <div className="flex items-center space-x-1">
+        <SketchButtons iconOnly={true} />
+      </div>
+    </div>
+  );
+}
